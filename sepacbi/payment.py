d391c6214e8ddd278698997ff4de958306de0d15
ePezhman <epezhman@gmail.com>
2015-03-23 16:52:10 +0100

    header change

--- sepacbi/payment.py
+++ sepacbi/payment.py
@@ -148,14 +148,12 @@ class Payment(AttributeCarrier):
         `envelope` attribute.
         """
         xsi = 'http://www.w3.org/2001/XMLSchema-instance'
-        # if self.envelope:
-        #     tag = 'CBIBdyPaymentRequest'
-        # else:
-        #     tag = 'CBIPaymentRequest'
+        if self.envelope:
+            tag = 'CBIBdyPaymentRequest'
+        else:
+            tag = 'CBIPaymentRequest'
         # schema = tag + '.00.04.00'
         # xmlns = 'urn:CBI:xsd:' + schema
-
-        tag = 'Document'
         xmlns = 'urn:iso:std:iso:20022:tech:xsd:pain.001.003.03'
         # schema_location = xmlns + ' ' + schema + '.xsd'
         root = etree.Element(
